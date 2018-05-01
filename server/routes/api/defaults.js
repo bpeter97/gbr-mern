5f94df325700d37fd8014547eac5e1442cf3b1f1
bpeter97 <brian.lee.peter@gmail.com>
2018-04-30 23:41:29 -0700

    Add defaults routes for api, these are routes immediately after url/api/

---
+++ server/routes/api/defaults.js
@@ -0,0 +1,10 @@
+const express = require("express");
+const router = express.Router();
+
+// @route   POST api/login
+// @desc    Logs a user in.
+// @access  Private
+
+// @route   POST api/register
+// @desc    Registers a new user.
+// @access  Private
