0c28ce582656d82d986c9a64162bca129e19931a
bpeter97 <brian.lee.peter@gmail.com>
2018-04-30 23:36:50 -0700

    Add products route for api.

---
+++ server/routes/api/products.js
@@ -0,0 +1,22 @@
+const express = require("express");
+const router = express.Router();
+
+// @route   GET api/products/
+// @desc    Retrieves all of the products
+// @access  Private
+
+// @route   POST api/products/
+// @desc    Creates a new product.
+// @access  Private
+
+// @route   GET api/products/:id
+// @desc    Retrieves a single product.
+// @access  Private
+
+// @route   PATCH api/products/:id
+// @desc    Updates all or part of a single product.
+// @access  Private
+
+// @route   DELETE api/products/:id
+// @desc    Deletes a single product from the database.
+// @access  Private
