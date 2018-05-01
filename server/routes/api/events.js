e4ab1b645602af8fdb9eaa37c3cb2e1200f5f46b
bpeter97 <brian.lee.peter@gmail.com>
2018-04-30 23:37:29 -0700

    Add events route for api.

---
+++ server/routes/api/events.js
@@ -0,0 +1,22 @@
+const express = require("express");
+const router = express.Router();
+
+// @route   GET api/events/
+// @desc    Retrieves all of the events
+// @access  Private
+
+// @route   POST api/events/
+// @desc    Creates a new event.
+// @access  Private
+
+// @route   GET api/events/:id
+// @desc    Retrieves a single event.
+// @access  Private
+
+// @route   PATCH api/events/:id
+// @desc    Updates all or part of a single event.
+// @access  Private
+
+// @route   DELETE api/events/:id
+// @desc    Deletes a single event from the database.
+// @access  Private
