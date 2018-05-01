cafb8235055671ae97b9179a934e65c9b0b38348
bpeter97 <brian.lee.peter@gmail.com>
2018-04-30 23:32:42 -0700

    Add users route for api.

---
+++ server/routes/api/users.js
@@ -0,0 +1,22 @@
+const express = require("express");
+const router = express.Router();
+
+// @route   GET api/users/
+// @desc    Retrieves all of the users
+// @access  Private
+
+// @route   POST api/users/
+// @desc    Registers a new user.
+// @access  Private
+
+// @route   GET api/users/:id
+// @desc    Retrieves a single users information.
+// @access  Private
+
+// @route   PATCH api/users/:id
+// @desc    Updates all or part of a single users information.
+// @access  Private
+
+// @route   DELETE api/users/:id
+// @desc    Deletes a single user from the database.
+// @access  Private
