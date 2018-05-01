0593ed70466adc2c6c83adbff7d4bbd41b0eacd2
bpeter97 <brian.lee.peter@gmail.com>
2018-04-30 23:38:46 -0700

    Add orders route for api.

---
+++ server/routes/api/orders.js
@@ -0,0 +1,22 @@
+const express = require("express");
+const router = express.Router();
+
+// @route   GET api/orders/
+// @desc    Retrieves all of the orders
+// @access  Private
+
+// @route   POST api/orders/
+// @desc    Creates a new order.
+// @access  Private
+
+// @route   GET api/orders/:id
+// @desc    Retrieves a single order.
+// @access  Private
+
+// @route   PATCH api/orders/:id
+// @desc    Updates all or part of a single order.
+// @access  Private
+
+// @route   DELETE api/orders/:id
+// @desc    Deletes a single order from the database.
+// @access  Private
