ea30130b252202c00311e543d815572f71d6bec5
bpeter97 <brian.lee.peter@gmail.com>
2018-04-30 23:38:18 -0700

    Add quotes route for api.

---
+++ server/routes/api/quotes.js
@@ -0,0 +1,22 @@
+const express = require("express");
+const router = express.Router();
+
+// @route   GET api/quotes/
+// @desc    Retrieves all of the quotes
+// @access  Private
+
+// @route   POST api/quotes/
+// @desc    Creates a new quote.
+// @access  Private
+
+// @route   GET api/quotes/:id
+// @desc    Retrieves a single quote.
+// @access  Private
+
+// @route   PATCH api/quotes/:id
+// @desc    Updates all or part of a single quote.
+// @access  Private
+
+// @route   DELETE api/quotes/:id
+// @desc    Deletes a single quote from the database.
+// @access  Private
