145c9c4a78714882b32037263145d554a8b1776a
bpeter97 <brian.lee.peter@gmail.com>
2018-04-30 23:34:38 -0700

    Add containers route for api.

---
+++ server/routes/api/containers.js
@@ -0,0 +1,22 @@
+const express = require("express");
+const router = express.Router();
+
+// @route   GET api/containers/
+// @desc    Retrieves all of the containers
+// @access  Private
+
+// @route   POST api/containers/
+// @desc    Creates a new container.
+// @access  Private
+
+// @route   GET api/containers/:id
+// @desc    Retrieves a single container.
+// @access  Private
+
+// @route   PATCH api/containers/:id
+// @desc    Updates all or part of a single container.
+// @access  Private
+
+// @route   DELETE api/containers/:id
+// @desc    Deletes a single container from the database.
+// @access  Private
