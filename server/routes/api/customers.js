13ff79dde6142f5c02388a65d817febaf8af77f9
bpeter97 <brian.lee.peter@gmail.com>
2018-04-30 23:36:07 -0700

    Add customers route for api.

---
+++ server/routes/api/customers.js
@@ -0,0 +1,22 @@
+const express = require("express");
+const router = express.Router();
+
+// @route   GET api/customers/
+// @desc    Retrieves all of the customers
+// @access  Private
+
+// @route   POST api/customers/
+// @desc    Creates a new customer.
+// @access  Private
+
+// @route   GET api/customers/:id
+// @desc    Retrieves a single customer.
+// @access  Private
+
+// @route   PATCH api/customers/:id
+// @desc    Updates all or part of a single customer.
+// @access  Private
+
+// @route   DELETE api/customers/:id
+// @desc    Deletes a single customer from the database.
+// @access  Private
