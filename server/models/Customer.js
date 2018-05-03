c9b6daa2a4fd11a24f76b694cb56943743f03070
Taylor Hartley <twh0008@auburn.edu>
2018-05-03 16:30:16 -0500

    Add Customer schema

---
+++ server/models/Customer.js
@@ -0,0 +1,57 @@
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+
+const CustomerSchema = new Schema({
+  name: {
+    type: String,
+    required: true
+  },
+  address1: {
+    type: String,
+    required: true
+  },
+  address2: {
+    type: String
+  },
+  city: {
+    type: String,
+    required: true
+  },
+  zipcode: {
+    type: String,
+    required: true
+  },
+  state: {
+    type: String,
+    required: true
+  },
+  phone: {
+    type: String,
+    required: true
+  },
+  ext: {
+    type: String
+  },
+  fax: {
+    type: String
+  },
+  email: {
+    type: String
+  },
+  rdp: {
+    type: String
+  },
+  notes: {
+    type: String
+  },
+  isFlagged: {
+    type: Boolean,
+    default: false,
+    required: true
+  },
+  flagReason: {
+    type: String
+  }
+});
+
+module.exports = Customer = mongoose.model("Customer", CustomerSchema);
