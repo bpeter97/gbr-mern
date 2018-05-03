a761899904b3a46dc0146ab5d1ee828a791db29d
Taylor Hartley <twh0008@auburn.edu>
2018-05-03 16:19:40 -0500

    Add ContainerSize schema

---
+++ server/models/ContainerSize.js
@@ -0,0 +1,11 @@
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+
+const ContainerSizeSchema = new Schema({
+    size: {
+        type: String,
+        required: true
+    }
+});
+
+module.exports = ContainerSize = mongoose.model("ContainerSize", ContainerSizeSchema);
