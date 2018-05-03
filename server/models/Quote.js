90fac324befb5ff91581363fb89c49c5de880bc0
Taylor Hartley <twh0008@auburn.edu>
2018-05-03 17:01:46 -0500

    Add Quote schema

---
+++ server/models/Quote.js
@@ -0,0 +1,46 @@
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+
+const QuoteSchema = new Schema({
+  customer: {
+    type: Schema.Types.ObjectId,
+    required: true
+  },
+  purchaseType: {
+    type: Schema.Types.ObjectId,
+    required: true
+  },
+  creationDate: {
+    type: String,
+    required: true
+  },
+  expirationDate: {
+    type: String,
+    required: true
+  },
+  status: {
+    type: String,
+    default: Open
+  },
+  attention: {
+    type: String
+  },
+  isHidden: {
+    type: Boolean,
+    default: false
+  },
+  purchasePrices: {
+    type: Schema.Types.ObjectId,
+    required: true
+  },
+  products: [
+    {
+      type: Schema.Types.ObjectId
+    }
+  ],
+  createdBy: {
+    type: Schema.Types.ObjectId,
+    required: true
+  }
+});
+module.exports = Quote = mongoose.model("Quote", QuoteSchema);
