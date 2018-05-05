57acb58a18f4c32696d308ee8b74af8fe93d8865
bpeter97 <brian.lee.peter@gmail.com>
2018-05-05 13:48:57 -0700

    Modify container and container stats model and add node-geocoder for getting lat and lon

--- server/models/Container.js
+++ server/models/Container.js
@@ -2,57 +2,55 @@ const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
 const ContainerSchema = new Schema({
-    gbrNumber: {
-        type: String
-    },
-    releaseNumber: {
-        type: String
-    },
-    size: {
-        type: Schema.Types.ObjectId,
-        required: true
-    },
-    serialNumber: {
-        type: String,
-        required: true
-    },
-    hasShelves: {
-        type: Boolean,
-        default: false
-    },
-    isPainted: {
-        type: Boolean,
-        default: false
-    },
-    hasOnboxNumbers: {
-        type: Boolean,
-        default: false
-    },
-    hasSigns: {
-        type: Boolean,
-        default: false
-    },
-    rentalResale: {
-        type: String,
-        minlength: 6,
-        maxlength: 6,
-        required: true
-    },
-    isFlagged: {
-        type: Boolean,
-        default: false
-    },
-    flagReason: {
-        type: String
-    },
-    shortName: {
-        type: Schema.Types.ObjectId,
-        required: true
-    },
-    stats: {
-        type: Schema.Types.ObjectId,
-        required: true
-    }
+  gbrNumber: {
+    type: String
+  },
+  releaseNumber: {
+    type: String
+  },
+  size: {
+    type: Schema.Types.ObjectId,
+    required: true
+  },
+  serialNumber: {
+    type: String,
+    required: true
+  },
+  hasShelves: {
+    type: Boolean,
+    default: false
+  },
+  isPainted: {
+    type: Boolean,
+    default: false
+  },
+  hasOnboxNumbers: {
+    type: Boolean,
+    default: false
+  },
+  hasSigns: {
+    type: Boolean,
+    default: false
+  },
+  rentalResale: {
+    type: String,
+    required: true
+  },
+  isFlagged: {
+    type: Boolean,
+    default: false
+  },
+  flagReason: {
+    type: String
+  },
+  shortName: {
+    type: Schema.Types.ObjectId,
+    required: true
+  },
+  stats: {
+    type: Schema.Types.ObjectId,
+    required: true
+  }
 });
 
 module.exports = Container = mongoose.model("Container", ContainerSchema);
