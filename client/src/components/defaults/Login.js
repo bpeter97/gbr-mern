8452d4532f4337d699402357ea0dc2a2c396ee8f
Taylor Hartley <twh0008@auburn.edu>
2018-05-05 01:00:35 -0500

    Change class to className in Login.js

--- client/src/components/defaults/Login.js
+++ client/src/components/defaults/Login.js
@@ -34,13 +34,13 @@ class Login extends Component {
     const { errors } = this.state;
 
     return (
-      <div class="container-fluid">
-        <div class="row mh-100vh">
+      <div className="container-fluid">
+        <div className="row mh-100vh">
           <div
-            class="col-10 col-sm-8 col-md-6 col-lg-6 offset-1 offset-sm-2 offset-md-3 offset-lg-0 align-self-center d-lg-flex align-items-lg-center align-self-lg-stretch bg-white p-5 rounded rounded-lg-0 my-5 my-lg-0"
+            className="col-10 col-sm-8 col-md-6 col-lg-6 offset-1 offset-sm-2 offset-md-3 offset-lg-0 align-self-center d-lg-flex align-items-lg-center align-self-lg-stretch bg-white p-5 rounded rounded-lg-0 my-5 my-lg-0"
             id="login-block">
-            <div class="m-auto w-lg-75 w-xl-50">
-              <h2 class="text-center font-weight-light mb-5">
+            <div className="m-auto w-lg-75 w-xl-50">
+              <h2 className="text-center font-weight-light mb-5">
                 GBR Management System
               </h2>
               <form>
@@ -60,18 +60,18 @@ class Login extends Component {
                   onChange={this.onChange}
                   error={errors.password}
                 />
-                <button class="btn btn-info mt-2" type="submit">
+                <button className="btn btn-info mt-2" type="submit">
                   Log In
                 </button>
               </form>
-              <p class="mt-3 mb-0">
-                <a href="#" class="small">
+              <p className="mt-3 mb-0">
+                <a href="#" className="small">
                   Forgot your email or password?
                 </a>
               </p>
             </div>
           </div>
-          <div class="col-lg-6 d-flex align-items-end" id="bg-block" />
+          <div className="col-lg-6 d-flex align-items-end" id="bg-block" />
         </div>
       </div>
     );
