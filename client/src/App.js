4d3d77edaf5dc3967939dd51954fc6ddd4a82a1a
Taylor Hartley <twh0008@auburn.edu>
2018-05-05 19:44:38 -0500

    Add react, add actions, reducers, components

--- client/src/App.js
+++ client/src/App.js
@@ -1,19 +1,47 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import jwt_decode from "jwt-decode";
+import setAuthToken from "./utils/setAuthToken";
+import { setCurrentUser, logoutUser } from "./actions/defaultsActions";
+
+
+import { Provider } from "react-redux";
+import store from "./store";
+
+import Login from './components/defaults/Login';
+import PrivateRoute from "./components/common/PrivateRoute";
+import Dashboard from './components/dashboard';
+
+
+
+import "./App.css";
+
+
+if (localStorage.jwtToken) {
+  setAuthToken(localStorage.jwtToken);
+  const decoded = jwt_decode(localStorage.jwtToken);
+  store.dispatch(setCurrentUser(decoded));
+  const currentTime = Date.now() / 1000;
+  if (decoded.exp < currentTime) {
+    store.dispatch(logoutUser());
+    window.location.href = '/login';
+  }
+}
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
+      <Provider store={store}>
+        <Router>
+          <div className="App">
+            <Route exact path="/" component={Dashboard} />
+              <Route exact path="/login" component={Login} />
+              <Switch>
+                <PrivateRoute exact path="/dashboard" component={Dashboard} />
+              </Switch>
+          </div>
+        </Router>
+      </Provider>
     );
   }
 }
---
+++ client/src/components/defaults/Login.js
@@ -0,0 +1,105 @@
+import React, { Component } from "react";
+import { connect } from "react-redux";
+import PropTypes from "prop-types";
+import TextFieldGroup from "../common/TextFieldGroup";
+import { loginUser } from "../../actions/defaultsActions";
+
+class Login extends Component {
+  constructor() {
+    super();
+    this.state = {
+      username: "",
+      password: "",
+      errors: {}
+    };
+    this.onChange = this.onChange.bind(this);
+    this.onSubmit = this.onSubmit.bind(this);
+  }
+
+  componentDidMount() {
+    if (this.props.auth.isAuthenticated) {
+      this.props.history.push("/dashboard");
+    }
+  }
+
+  componentWillReceiveProps(nextProps) {
+    if (nextProps.auth.isAuthenticated) {
+      this.props.history.push("/dashboard");
+    }
+    if (nextProps.errors) {
+      this.setState({ error: nextProps.errors });
+    }
+  }
+
+  onSubmit(e) {
+    e.preventDefault();
+
+    const userData = {
+      username: this.state.username,
+      password: this.state.password
+    };
+    this.props.loginUser(userData);
+  }
+
+  onChange(e) {
+    this.setState({ [e.target.name]: e.target.value });
+  }
+  render() {
+    const { errors } = this.state;
+
+    return (
+      <div className="container-fluid">
+        <div className="row mh-100vh">
+          <div
+            className="col-10 col-sm-8 col-md-6 col-lg-6 offset-1 offset-sm-2 offset-md-3 offset-lg-0 align-self-center d-lg-flex align-items-lg-center align-self-lg-stretch bg-white p-5 rounded rounded-lg-0 my-5 my-lg-0"
+            id="login-block"
+          >
+            <div className="m-auto w-lg-75 w-xl-50">
+              <h2 className="text-center font-weight-light mb-5">
+                GBR Management System
+              </h2>
+              <form onSubmit={this.onSubmit}>
+                <TextFieldGroup
+                  placeholder="Username"
+                  name="username"
+                  type="username"
+                  value={this.state.username}
+                  onChange={this.onChange}
+                  error={errors.username}
+                />
+                <TextFieldGroup
+                  placeholder="Password"
+                  name="password"
+                  type="password"
+                  value={this.state.password}
+                  onChange={this.onChange}
+                  error={errors.password}
+                />
+                <input type="submit" className="btn btn-info mt-2" />
+              </form>
+              <p className="mt-3 mb-0">
+                <a href="#" className="small">
+                  Forgot your email or password?
+                </a>
+              </p>
+            </div>
+          </div>
+          <div className="col-lg-6 d-flex align-items-end" id="bg-block" />
+        </div>
+      </div>
+    );
+  }
+}
+
+Login.PropTypes = {
+  loginUser: PropTypes.func.isRequired,
+  auth: PropTypes.object.isRequired,
+  errors: PropTypes.object.isRequired
+};
+
+const mapStateToProps = state => ({
+  auth: state.auth,
+  errors: state.errors
+});
+
+export default connect(mapStateToProps, { loginUser })(Login);
