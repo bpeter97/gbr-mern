a7c19343bbaf9ed8b92f6a8ac3036deb750acbce
Taylor Hartley <twh0008@auburn.edu>
2018-05-05 22:07:50 -0500

    Create wireframe dashboard

--- client/src/components/dashboard/index.js
+++ client/src/components/dashboard/index.js
@@ -1,12 +1,23 @@
-import React from "react";
-import { bindActionCreators } from "redux";
+import React, { Component } from "react";
 import { connect } from "react-redux";
+import PropTypes from "prop-types";
+import { logoutUser } from "../../actions/defaultsActions";
+import Navbar from "../navbar";
 
+class Dashboard extends Component {
+  constructor() {
+    super();
+    this.state = {};
+  }
 
-const Dashboard = props => (
-  <div>
-    <h1>DASHBOARD PAGE</h1>
-  </div>
-);
+  render() {
+    return (
+      <div>
+        <Navbar />
+        <h1>DASHBOARD PAGE!</h1>
+      </div>
+    );
+  }
+}
 
 export default Dashboard;
