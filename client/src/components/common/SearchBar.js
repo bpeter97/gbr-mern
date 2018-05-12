b1c59e53bea303913017f2abc41d8763041837fd
Taylor Hartley <twh0008@auburn.edu>
2018-05-11 21:06:56 -0500

    Add SearchBar component

---
+++ client/src/components/common/SearchBar.js
@@ -0,0 +1,41 @@
+import React from "react";
+import classnames from "classnames";
+import PropTypes from "prop-types";
+
+const SearchBar = ({
+  name,
+  placeholder,
+  value,
+  className,
+  label,
+  type,
+  onChange,
+  disabled
+}) => {
+  return (
+    <input
+      type={type}
+      className={className}
+      placeholder={placeholder}
+      name={name}
+      value={value}
+      onChange={onChange}
+      disabled={disabled}
+    />
+  );
+};
+
+SearchBar.propTypes = {
+  name: PropTypes.string.isRequired,
+  placeholder: PropTypes.string,
+  value: PropTypes.string.isRequired,
+  type: PropTypes.string.isRequired,
+  onChange: PropTypes.func.isRequired,
+  disabled: PropTypes.string
+};
+
+SearchBar.defaultProps = {
+  type: "text"
+};
+
+export default SearchBar;
