8e83a853c555f7d355420f463b0aa2d1b8438d1d
bpeter97 <brian.lee.peter@gmail.com>
2018-05-12 20:35:38 -0700

    Add the rest of the functions for the settings routes

--- server/helpers/settings.js
+++ server/helpers/settings.js
@@ -24,7 +24,24 @@ exports.getPurchaseTypes = (req, res) => {
 // @route   POST api/settings/purchasetypes
 // @desc    Creates a new purchase type
 // @access  Private
-exports.postPurchaseType = (req, res) => {};
+exports.postPurchaseType = (req, res) => {
+  // Fetch validation errors.
+  const { errors, isValid } = validateSettingInput("purchaseType", req.body);
+
+  // send 400 error with validation errors if not valid.
+  if (!isValid) return res.status(400).json(errors);
+
+  // create it.
+  var newType = new PurchaseType({ type: req.body.type });
+
+  // Save it in the DB and return it.
+  newType
+    .save()
+    .then(purchaseType => {
+      res.json({ purchaseType });
+    })
+    .catch(e => console.log(e));
+};
 
 // @route   GET api/settings/purchasetypes/:id
 // @desc    Retrieves a purchase type
@@ -54,9 +71,58 @@ exports.getPurchaseType = (req, res) => {
 // @route   PATCH api/settings/purchasetypes/:id
 // @desc    Updates a purchase type
 // @access  Private
-exports.patchPurchaseType = (req, res) => {};
+exports.patchPurchaseType = (req, res) => {
+  // Fetch validation errors.
+  const { errors, isValid } = validateSettingInput("purchaseType", req.body);
+
+  // send 400 error with validation errors if not valid.
+  if (!isValid || !ObjectID.isValid(req.params.id)) {
+    if (!ObjectID.isValid(req.params.id)) {
+      errors.purchaseType = "No purchase type found with that ID in the URL";
+    }
+    return res.status(400).json(errors);
+  }
+
+  // find that document and update the type field.
+  PurchaseType.findByIdAndUpdate(
+    req.params.id,
+    { $set: { type: type } },
+    { new: true }
+  )
+    .then(purchaseType => {
+      if (!purchaseType) {
+        errors.purchaseType = "Unable to find and update the purchaseType";
+        return res.status(404).json(errors);
+      }
+      // Return the newly modified purchaseType.
+      res.json({ purchaseType });
+    })
+    .catch(e => res.status(400).send());
+};
 
 // @route   DELETE api/settings/purchasetypes/:id
 // @desc    Deletes a purchase type
 // @access  Private
-exports.deletePurchaseType = (req, res) => {};
+exports.deletePurchaseType = (req, res) => {
+  let errors = {};
+
+  // send 400 error with validation errors if not valid.
+  if (!isValid || !ObjectID.isValid(req.params.id)) {
+    if (!ObjectID.isValid(req.params.id)) {
+      errors.purchaseType = "No purchase type found with that ID in the URL";
+    }
+    return res.status(400).json(errors);
+  }
+
+  // find and delete the purchase type
+  PurchaseType.findByIdAndRemove(req.params.id)
+    .then(purchaseType => {
+      if (!purchaseType) {
+        errors.purchaseType = "Unable to find and remove the purchase type";
+        res.status(404).json(errors);
+      }
+      // Return the product that was just removed.
+      res.json({ purchaseType });
+    })
+    .catch(e => console.log(e));
+};
