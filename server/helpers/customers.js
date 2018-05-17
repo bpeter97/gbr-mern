f2a7129dd9a5e2848f0bf231ae4d846088837755
bpeter97 <brian.lee.peter@gmail.com>
2018-05-17 09:33:21 -0700

    Add functionality to the customer helpers

--- server/helpers/customers.js
+++ server/helpers/customers.js
@@ -6,29 +6,155 @@ const isEmpty = require("./../validation/is-empty");
 const Customer = require("./../models/Customer");
 
 // validation files
-// const validateCalendarInput = require("../validation/calendar");
+// const validateCustomerInput = require("../validation/customer");
 
 // @route   GET api/customers/
 // @desc    Retrieves all of the customers
 // @access  Private
-exports.getCustomers = (req, res) => {};
+exports.getCustomers = (req, res) => {
+  Customer.find({})
+    .then(customers => {
+      if (!customers) {
+        return res.status(400).json({ error: "No customers found" });
+      }
+      res.json({ customers });
+    })
+    .catch(e => res.status(404).json(e));
+};
 
 // @route   POST api/customers/
 // @desc    Creates a new customer.
 // @access  Private
-exports.postCustomer = (req, res) => {};
+exports.postCustomer = (req, res) => {
+  // check for validation errors
+  const { errors, isValid } = validateCustomerInput(req.body);
+
+  // send 400 error with validation errors if not valid.
+  if (!isValid) return res.status(400).json(errors);
+
+  // Set the properties for the new customer
+  var body = _.pick(req.body, [
+    "name",
+    "address1",
+    "address2",
+    "city",
+    "zipcode",
+    "state",
+    "phone",
+    "ext",
+    "fax",
+    "email",
+    "rdp",
+    "notes",
+    "isFlagged",
+    "flagReason"
+  ]);
+
+  // save the customer
+  var newCustomer = new Customer(body);
+
+  newCustomer
+    .save()
+    .then(customer => {
+      if (!customer) {
+        errors.customers = "Unable to create the new customer";
+        return res.status(400).json(errors);
+      }
+      res.json({ customer });
+    })
+    .catch(e => console.log(e));
+};
 
 // @route   GET api/customers/:id
 // @desc    Retrieves a single customer.
 // @access  Private
-exports.getCustomer = (req, res) => {};
+exports.getCustomer = (req, res) => {
+  let errors = {};
+
+  // check to see if the ID passed in is a valid ObjectID
+  if (!ObjectID.isValid(req.params.id)) {
+    errors.customer = "There was no customer found";
+    return res.status(400).json(errors);
+  }
+
+  // find the customer and return results
+  Customer.findById(req.params.id)
+    .then(customer => {
+      if (!customer) {
+        errors.customer = "There was no customer found";
+        return res.status(400).json(errors);
+      }
+      res.json({ customer });
+    })
+    .catch(e => console.log(e));
+};
 
 // @route   PATCH api/customers/:id
 // @desc    Updates all or part of a single customer.
 // @access  Private
-exports.patchCustomer = (req, res) => {};
+exports.patchCustomer = (req, res) => {
+  const { errors, isValid } = validateCustomerInput(req.body);
+
+  // send 400 error with validation errors if not valid.
+  if (!isValid || !ObjectID.isValid(req.params.id)) {
+    if (!ObjectID.isValid(req.params.id)) {
+      errors.customer = "There was no customer found";
+    }
+    return res.status(400).json(errors);
+  }
+
+  var body = _.pick(req.body, [
+    "name",
+    "address1",
+    "address2",
+    "city",
+    "zipcode",
+    "state",
+    "phone",
+    "ext",
+    "fax",
+    "email",
+    "rdp",
+    "notes",
+    "isFlagged",
+    "flagReason"
+  ]);
+
+  // finally, update the customer
+  Customer.findByIdAndUpdate(req.params.id, { $set: body }, { new: true })
+    .then(customer => {
+      if (!customer) {
+        errors.customer = "Unable to update the customer";
+        return res.status(400).json(errors);
+      }
+
+      res.json({ customer });
+    })
+    .catch(e => console.log(e));
+};
 
 // @route   DELETE api/customers/:id
 // @desc    Deletes a single customer from the database.
 // @access  Private
-exports.deleteCustomer = (req, res) => {};
+exports.deleteCustomer = (req, res) => {
+  let errors = {};
+
+  // Check to see if id is a valid ObjectID
+  if (!ObjectID.isValid(req.params.id)) {
+    errors.customer = "There was no customer found";
+    return res.status(400).json(errors);
+  }
+
+  // Find the customer by ID and remove it.
+  Customer.findByIdAndRemove(req.params.id)
+    .then(customer => {
+      // customer was not found!
+      if (!customer) {
+        errors.customer = "Unable to find and remove the customer";
+        res.status(404).json(errors);
+      }
+      // Return the customer that was just removed.
+      res.json({ customer });
+    })
+    .catch(e => res.status(400).send());
+};
