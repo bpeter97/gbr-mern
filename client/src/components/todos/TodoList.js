dbe930430680bf64d04899ae0287efcaa6e0b7b6
Taylor Hartley <twh0008@auburn.edu>
2018-05-20 15:37:03 -0500

    Add TodoList component

--- client/src/components/todos/TodoList.js
+++ client/src/components/todos/TodoList.js
@@ -3,83 +3,18 @@ import { connect } from "react-redux";
 import SvgIcon from "react-icons-kit";
 import PropTypes from "prop-types";
 import TextFieldInput from "../common/TextFieldInput";
-import Todo from "./Todo";
+import TodoForm from "./TodoForm";
+import TodoItem from "./TodoItem";
 
 class TodoList extends Component {
-  constructor() {
-    super();
-    this.state = {
-      todoDesc: "",
-      errors: {}
-    };
-
-    this.onSubmit = this.onSubmit.bind(this);
-    this.onChange = this.onChange.bind(this);
-  }
-
-  componentDidMount() {
-    //fetch data from auth.user.todos
-  }
-
-  componentWillReceiveProps(nextProps) {
-    if (nextProps.errors) {
-      this.setState({ errors: nextProps.errors });
-    }
-  }
-
-  onSubmit(e) {
-    e.preventDefault();
-    const todoData = {
-      todoDesc: this.state.todoDesc
-    };
-    debugger;
-    //send to backend
-    //attach to user object
-    // this.props.createTodo(todoData);
-  }
-
-  onChange(e) {
-    this.setState({ [e.target.name]: e.target.value });
-  }
-
   render() {
-    const { errors } = this.state;
-    return (
-      <div className="todo-list-container">
-        <form className="todo-form" onSubmit={this.onSubmit}>
-          <TextFieldInput
-            placeholder="Add Todo..."
-            className="new-todo-input"
-            name="todoDesc"
-            type="text"
-            value={this.state.todoDesc}
-            onChange={this.onChange}
-            autoComplete={"off"}
-            error={errors.todo}
-          />
-        </form>
-
-        <ul className="todo-list">
-          {/* append todos */}
-          {/* <Todo todo={todo} */}
-          <Todo desc="Todo #1" />
-          <Todo desc="Todo #2" />
-        </ul>
-      </div>
-    );
+    const { todos } = this.props;
+    return todos.map(todo => <TodoItem key={todo._id} todo={todo} />);
   }
 }
 
 TodoList.propTypes = {
-  auth: PropTypes.object.isRequired,
-  errors: PropTypes.object.isRequired
+  todos: PropTypes.array.isRequired
 };
 
-const mapStateToProps = state => ({
-  auth: state.auth,
-  errors: state.errors,
-  location: state.router.location
-});
-
-export default connect(mapStateToProps)(TodoList);
-// export default connect(mapStateToProps, {createTodo})(TodoList);
+export default TodoList;
