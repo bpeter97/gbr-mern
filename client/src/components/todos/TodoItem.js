ad79f87d238fa8942116e4aa3abb9ae27e3417f2
Taylor Hartley <twh0008@auburn.edu>
2018-05-20 15:36:50 -0500

    Add TodoItem component

---
+++ client/src/components/todos/TodoItem.js
@@ -0,0 +1,43 @@
+import React, { Component } from "react";
+import { connect } from "react-redux";
+import SvgIcon from "react-icons-kit";
+import TextFieldGroup from "../common/TextFieldGroup";
+import PropTypes from "prop-types";
+
+class TodoItem extends Component {
+  onDeleteClick(id) {
+    this.props.deleteTodo(id);
+  }
+
+  onCompleteClick(id) {
+    this.props.completeTodo(id);
+  }
+
+  render() {
+    const { todo } = this.props;
+
+    let newtodo = "";
+    return (
+      <div>
+        <li className="todo">
+          {todo.desc}
+          <span onClick={this.onDeleteClick.bind(this, todo._id)}>X</span>
+        </li>
+      </div>
+    );
+  }
+}
+
+TodoItem.propTypes = {
+  todo: PropTypes.object.isRequired,
+  deleteTodo: PropTypes.func.isRequired,
+  completeTodo: PropTypes.func.isRequired
+};
+
+const mapStateToProps = state => ({
+  auth: state.auth,
+  errors: state.errors,
+  location: state.router.location
+});
+
+export default connect(mapStateToProps)(TodoItem);
