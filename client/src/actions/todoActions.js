99cbb47773feca766fd1f29a86267abdb3acbd1c
Taylor Hartley <twh0008@auburn.edu>
2018-05-20 15:39:48 -0500

    Add todoActions

---
+++ client/src/actions/todoActions.js
@@ -0,0 +1,84 @@
+import axios from "axios";
+
+import {
+  ADD_TODO,
+  GET_ERRORS,
+  CLEAR_ERRORS,
+  GET_TODOS,
+  GET_TODO,
+  COMPLETE_TODO
+} from "./types";
+
+// Add Post
+export const addTodo = todoData => dispatch => {
+  //   dispatch(clearErrors());
+  //   axios
+  //     .post("/api/users", todoData)
+  //     .then(res =>
+  //       dispatch({
+  //         type: ADD_TODO,
+  //         payload: res.data
+  //       })
+  //     )
+  //     .catch(err =>
+  //       dispatch({
+  //         type: GET_ERRORS,
+  //         payload: err.response.data
+  //       })
+  //     );
+};
+
+// Get Todos
+export const getTodos = () => dispatch => {
+  let todos = [];
+  //   axios
+  //     .get("/api/Todos")
+  //     .then(res =>
+  //       dispatch({
+  //         type: GET_TODOS,
+  //         payload: res.data
+  //       })
+  //     )
+  //     .catch(err =>
+  //       dispatch({
+  //         type: GET_TODOS,
+  //         payload: null
+  //       })
+  //     );
+};
+
+// Get Post
+export const getTodo = id => dispatch => {
+  //   axios
+  //     .get(`/api/todos/${id}`)
+  //     .then(res =>
+  //       dispatch({
+  //         type: GET_TODO,
+  //         payload: res.data
+  //       })
+  //     )
+  //     .catch(err =>
+  //       dispatch({
+  //         type: GET_TODO,
+  //         payload: null
+  //       })
+  //     );
+};
+
+// Delete Post
+export const deleteTodo = id => dispatch => {
+  //   axios
+  //     .delete(`/api/todos/${id}`)
+  //     .then(res =>
+  //       dispatch({
+  //         type: DELETE_TODO,
+  //         payload: id
+  //       })
+  //     )
+  //     .catch(err =>
+  //       dispatch({
+  //         type: GET_ERRORS,
+  //         payload: err.response.data
+  //       })
+  //     );
+};
