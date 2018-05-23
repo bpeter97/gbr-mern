f95cf5ef57d76aa835f13ed83845c8a9d505d89e
Taylor Hartley <twh0008@auburn.edu>
2018-05-23 15:22:29 -0500

    Add todoReducer to state

--- client/src/reducers/index.js
+++ client/src/reducers/index.js
@@ -2,11 +2,13 @@ import { combineReducers } from "redux";
 import authReducer from "./authReducer";
 import errorReducer from "./errorReducer";
 import userReducer from "./userReducer";
+import todoReducer from "./todoReducer";
 import { routerReducer } from "react-router-redux";
 
 export default combineReducers({
   auth: authReducer,
   errors: errorReducer,
   router: routerReducer,
+  todos: todoReducer,
   users: userReducer
 });
