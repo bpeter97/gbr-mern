065c5daaaf9214fcc6402dc3e6e310ab5d7b126a
bpeter97 <brian.lee.peter@gmail.com>
2018-05-23 20:11:57 -0700

    Add routes for user profile

--- server/routes/api/defaults.js
+++ server/routes/api/defaults.js
@@ -3,6 +3,9 @@ const router = express.Router();
 
 const helpers = require("../../helpers/defaults");
 
+// middleware
+const authorization = require("./../../middleware/authorization");
+
 // @route   POST api/login
 // @desc    Logs a user in.
 // @access  Public
@@ -12,5 +15,14 @@ router.route("/login").post(helpers.login);
 // @desc    Registers a new user.
 // @access  Public
 router.route("/register").post(helpers.register);
+
+// @route   api/profile
+// @GET     Retrieves logged in users information.
+// @POST    Updates the logged in users information.
+// @access  Private
+router
+  .route("/profile")
+  .get(authorization, helpers.getProfile)
+  .patch(authorization, helpers.patchProfile);
 
 module.exports = router;
