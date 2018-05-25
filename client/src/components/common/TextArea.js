a156b5741910c4297c684e69d003a6ff15097eee
Taylor Hartley <twh0008@auburn.edu>
2018-05-24 21:13:51 -0500

    Add TextArea component

---
+++ client/src/components/common/TextArea.js
@@ -0,0 +1,36 @@
+import React from "react";
+import PropTypes from "prop-types";
+
+const TextArea = ({
+  name,
+  placeholder,
+  value,
+  className,
+  onChange,
+  disabled,
+  autoComplete
+}) => {
+  return (
+    <textarea
+      className={"form-control"}
+      placeholder={placeholder}
+      name={name}
+      onChange={onChange}
+      disabled={disabled}
+      autoComplete={autoComplete}
+      value={value}
+    />
+  );
+};
+
+TextArea.propTypes = {
+  name: PropTypes.string.isRequired,
+  placeholder: PropTypes.string,
+  value: PropTypes.string,
+  type: PropTypes.string.isRequired,
+  onChange: PropTypes.func.isRequired,
+  disabled: PropTypes.string,
+  autoComplete: PropTypes.string
+};
+
+export default TextArea;
