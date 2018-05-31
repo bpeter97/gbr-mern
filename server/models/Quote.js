ff5e94f53e38f21678124b9e6b71fc0ff6c9b945
bpeter97 <brian.lee.peter@gmail.com>
2018-05-31 13:38:15 -0700

    Modify Quote model to adjust how products are stored within it

--- server/models/Quote.js
+++ server/models/Quote.js
@@ -2,45 +2,53 @@ const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
 const QuoteSchema = new Schema({
-  customer: {
-    type: Schema.Types.ObjectId,
-    required: true
-  },
-  purchaseType: {
-    type: Schema.Types.ObjectId,
-    required: true
-  },
-  creationDate: {
-    type: String,
-    required: true
-  },
-  expirationDate: {
-    type: String,
-    required: true
-  },
-  status: {
-    type: String,
-    default: Open
-  },
-  attention: {
-    type: String
-  },
-  isHidden: {
-    type: Boolean,
-    default: false
-  },
-  purchasePrices: {
-    type: Schema.Types.ObjectId,
-    required: true
-  },
-  products: [
-    {
-      type: Schema.Types.ObjectId
-    }
-  ],
-  createdBy: {
-    type: Schema.Types.ObjectId,
-    required: true
-  }
+	customer: {
+		type: Schema.Types.ObjectId,
+		required: true
+	},
+	purchaseType: {
+		type: Schema.Types.ObjectId,
+		required: true
+	},
+	creationDate: {
+		type: String,
+		required: true
+	},
+	expirationDate: {
+		type: String,
+		required: true
+	},
+	status: {
+		type: String,
+		default: Open
+	},
+	attention: {
+		type: String
+	},
+	isHidden: {
+		type: Boolean,
+		default: false
+	},
+	purchasePrices: {
+		type: Schema.Types.ObjectId,
+		required: true
+	},
+	products: [
+		{
+			quantity: {
+				type: Number,
+				min: 1,
+				required: true
+			},
+			product: {
+				type: Schema.Types.ObjectId,
+				ref: "RequestedProduct"
+			}
+		}
+	],
+	createdBy: {
+		type: Schema.Types.ObjectId,
+		required: true
+	}
 });
 module.exports = Quote = mongoose.model("Quote", QuoteSchema);
