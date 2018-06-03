9c4b99f21a6ab547eb21038398bf2a84b728ca8e
bpeter97 <brian.lee.peter@gmail.com>
2018-06-03 00:03:08 -0700

    Add quote/customers/:id and quote/users/:id routes to quote routes

--- server/routes/api/quotes.js
+++ server/routes/api/quotes.js
@@ -9,9 +9,21 @@ const helpers = require("./../../helpers/quotes");
 // @POST    Creates a new quote
 // @access  Private
 router
-	.route("/")
-	.get(helpers.getQuotes)
-	.post(helpers.postQuote);
+  .route("/")
+  .get(helpers.getQuotes)
+  .post(helpers.postQuote);
+
+// @route   api/quotes/customer/:id
+// @GET     Retrieves all of a customer's quotes
+// @POST    Creates a new quote
+// @access  Private
+router.route("/customer/:id").get(helpers.getCustomerQuotes);
+
+// @route   api/quotes/user/:id
+// @GET     Retrieves all of a user's quotes
+// @POST    Creates a new quote
+// @access  Private
+router.route("/user/:id").get(helpers.getUserQuotes);
 
 // @route   api/quotes/:id
 // @GET     Retrieves a single quote
@@ -19,9 +31,9 @@ router
 // @DELETE  Deletes a single quote from the database
 // @access  Private
 router
-	.route("/:id")
-	.get(helpers.getQuote)
-	.patch(helpers.patchQuote)
-	.delete(helpers.deleteQuote);
+  .route("/:id")
+  .get(helpers.getQuote)
+  .patch(helpers.patchQuote)
+  .delete(helpers.deleteQuote);
 
 module.exports = router;
