7c3151ff1484bb8144df6525c8d65eabcdd9a17f
bpeter97 <brian.lee.peter@gmail.com>
2018-12-08 17:08:07 -0800

    Comment out some of the access control headers

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ require("dotenv").config();
 const express = require("express");
 const bodyParser = require("body-parser");
 const mongoose = require("mongoose");
+const path = require("path");
 
 // require the routes
 const defaults = require("./routes/api/defaults");
@@ -25,23 +26,26 @@ const port = process.env.PORT || 5000;
 
 const app = express();
 
-app.use(function(req, res, next) {
-  var allowedOrigins = [
-    "http://localhost:5000",
-    "http://localhost:3000",
-    "http://127.0.0.1:3000",
-    "http://localhost:9000"
-  ];
-  var origin = req.headers.origin;
-  if (allowedOrigins.indexOf(origin) > -1) {
-    res.setHeader("Access-Control-Allow-Origin", origin);
-  }
-  //res.header('Access-Control-Allow-Origin', 'http://127.0.0.1:8020');
-  res.header("Access-Control-Allow-Methods", "GET, OPTIONS");
-  res.header("Access-Control-Allow-Headers", "Content-Type, Authorization");
-  res.header("Access-Control-Allow-Credentials", true);
-  return next();
-});
+// Serve static files from the React app
+app.use(express.static(path.join(__dirname, "client/build")));
+
+// app.use(function(req, res, next) {
+//   var allowedOrigins = [
+//     "http://localhost:5000",
+//     "http://localhost:3000",
+//     "http://127.0.0.1:3000",
+//     "http://localhost:9000"
+//   ];
+//   var origin = req.headers.origin;
+//   if (allowedOrigins.indexOf(origin) > -1) {
+//     res.setHeader("Access-Control-Allow-Origin", origin);
+//   }
+//   //res.header('Access-Control-Allow-Origin', 'http://127.0.0.1:8020');
+//   res.header("Access-Control-Allow-Methods", "GET, OPTIONS");
+//   res.header("Access-Control-Allow-Headers", "Content-Type, Authorization");
+//   res.header("Access-Control-Allow-Credentials", true);
+//   return next();
+// });
 
 // Use the body-parser middleware
 app.use(bodyParser.urlencoded({ extended: false }));
